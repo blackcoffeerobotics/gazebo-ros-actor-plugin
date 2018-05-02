8280a6dcc1952cf68c111dc14c79772bf410b277
shtseng <q253upng@gmail.com>
2018-05-02 13:57:03 +0800

    Add cmd queue

--- include/gazebo_ros_actor_plugin/gazebo_ros_actor.h
+++ include/gazebo_ros_actor_plugin/gazebo_ros_actor.h
@@ -8,8 +8,8 @@
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <ros/callback_queue.h>
-#include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
 
 #include "gazebo/common/Plugin.hh"
 #include "gazebo/physics/physics.hh"
@@ -32,39 +32,31 @@ class GazeboRosActor : public ModelPlugin
 
     // Documentation Inherited.
   public: virtual void Reset();
-
-  private: void OnActive(const std_msgs::String::ConstPtr &msg);
   
   private: void VelCallback(const geometry_msgs::Twist::ConstPtr &msg);
+
+  private: void OdomCallback(const nav_msgs::Odometry::ConstPtr &msg);
 
     /// \brief Function that is called every update cycle.
     /// \param[in] _info Timing information
   private: void OnUpdate(const common::UpdateInfo &_info);
 
-  private: void QueueThread();
-
   private: void VelQueueThread();
+
+  private: void OdomQueueThread();
 
   private: void ReadTrajectoryFile();
 
   private: ros::NodeHandle *ros_node_;
 
-  private: ros::Subscriber sub_;
-  
   private: ros::Subscriber vel_sub_;
-
-  /// \brief Custom Callback Queue for activating actor
-  private: ros::CallbackQueue queue_;
 
   /// \brief Custom Callback Queue for guide vel
   private: ros::CallbackQueue vel_queue_;
 
   /// \brief Custom Callback Queue thread
-  private: boost::thread callbackQueueThread_;
 
   private: boost::thread velCallbackQueueThread_;
-
-  private: std::string topic_;
 
   private: std::string vel_topic_;
 
@@ -91,9 +83,6 @@ class GazeboRosActor : public ModelPlugin
   /// \brief Time of the last update.
   private: common::Time last_update;
 
-  /// \brief Time of the trajectory update.
-  private: common::Time start_time_;
-
   /// \brief Custom trajectory info.
   private: physics::TrajectoryInfoPtr trajectoryInfo;
 
@@ -106,9 +95,13 @@ class GazeboRosActor : public ModelPlugin
   /// \brief Queue data structure for storing the trajectory data
   private: std::queue<ignition::math::Pose3d> trajectory_;
 
+  private: std::queue<ignition::math::Vector3d> cmd_queue_;
+
   private: int trajectory_type_;
 
-  private: std::string nav_state_;
+  private: bool first_run_;
+
+  private: boost::mutex mutex_;
 };
 }
 #endif
